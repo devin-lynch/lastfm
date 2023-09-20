325005a212f64d962eaac757327bb01c334a9dce
devin-lynch <devin.l.lynch@gmail.com>
2023-09-20 16:13:55 -0500

    Implement basic styling to user page

--- app/_components/User.tsx
+++ app/_components/User.tsx
@@ -7,9 +7,9 @@ export default function User({ username, topArtists }) {
     displayTopArtists = topArtists.map((artist, i) => {
       return (
         <Link href={artist.url} key={i}>
-          <div>
-            <p>{artist.name}</p>
-            <p>{artist.playcount}</p>
+          <div className="mb-8 text-center">
+            <p className="text-2xl">{artist.name}</p>
+            <p>{artist.playcount} scrobbles</p>
           </div>
         </Link>
       );
@@ -18,8 +18,13 @@ export default function User({ username, topArtists }) {
 
   return (
     <div>
-      <p>{username}</p>
-      {displayTopArtists}
+      <Link href={`https://last.fm/user/${username}`}>
+        <p className="text-center text-4xl mb-8 mt-4">{username}</p>
+      </Link>
+      <div className="text-center">
+        <p className="mb-6 text-xl">Top Artists</p>
+        {displayTopArtists}
+      </div>
     </div>
   );
 }
